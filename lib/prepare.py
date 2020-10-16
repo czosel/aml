3423e6039d3c592eb892528aaccc6d423492326a
Christian Zosel <christian@zosel.ch>
2020-10-16 19:40:45 +0200

    dump/load

--- lib/prepare.py
+++ lib/prepare.py
@@ -1,6 +1,7 @@
 import numpy as np
 import joblib
 from time import time
+from pathlib import Path
 from sklearn.impute import SimpleImputer
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
@@ -15,6 +16,8 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import svm
+
+persistent = True
 
 
 def prepare_data(X_train_raw, X_test_raw, y_raw, select=True, clip=True):
@@ -41,32 +44,50 @@ def prepare_data(X_train_raw, X_test_raw, y_raw, select=True, clip=True):
         scaler = preprocessing.RobustScaler().fit(X)
         return scaler.transform(X), scaler.transform(X_test), y
 
+    n_features = 0.5
+    direction = "forward"
+    percentile = 20
+
     def select_percentile(data):
         nonlocal select
         if not select:
             return data
 
         X, X_test, y = data
-        select = SelectPercentile(mutual_info_regression, percentile=20).fit(X, y)
+
+        filename = f"joblib/percentile-{n_features}-out-of-{percentile}-percent-direction-{direction}.joblib"
+        if persistent and Path(filename).is_file():
+            print(f"loading percentile selection from cache: {filename}")
+            select = joblib.load(filename)
+        else:
+            select = SelectPercentile(mutual_info_regression, percentile=20).fit(X, y)
+            if persistent:
+                print("saving " + filename)
+                joblib.dump(select, filename)
+
         return select.transform(X), select.transform(X_test), y
 
     def select_greedy(data):
         X, X_test, y = data
-        svr = svm.SVR(kernel="rbf", C=100).fit(X, y)
-        tic = time()
-        n_features = 0.5
-        direction = "backward"
-        select = SequentialFeatureSelector(
-            svr, direction=direction, n_features_to_select=n_features, n_jobs=8
-        ).fit(X, y)
 
-        toc = time()
-        print(f"features selected: {select.get_support()}")
-        print(f"done in: {toc - tic:.2f}s")
-
-        percentile = 20
         filename = f"joblib/greedy-{n_features}-out-of-{percentile}-percent-direction-{direction}.joblib"
-        joblib.dump(select, filename)
+
+        if persistent and Path(filename).is_file():
+            print(f"loading greedy selection from cache: {filename}")
+            select = joblib.load(filename)
+        else:
+            svr = svm.SVR(kernel="rbf", C=100).fit(X, y)
+            tic = time()
+            select = SequentialFeatureSelector(
+                svr, direction=direction, n_features_to_select=n_features, n_jobs=8
+            ).fit(X, y)
+            toc = time()
+            print(f"features selected: {select.get_support()}")
+            print(f"done in: {toc - tic:.2f}s")
+
+            if persistent:
+                print("saving " + filename)
+                joblib.dump(select, filename)
 
         return select.transform(X), select.transform(X_test), y
 
