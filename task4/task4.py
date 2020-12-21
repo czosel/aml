9a4c3928b9cc4eced3ab7d290cca612d56f8322b
Christian Zosel <christian@zosel.ch>
2020-12-21 13:14:59 +0100

    use frequency bands in preprocessing

--- task4/task4.py
+++ task4/task4.py
@@ -11,6 +11,13 @@ X, X_test, y = load_data()
 info = mne.create_info(["eeg1", "eeg2"], 128, ["eeg", "eeg"])
 info2 = mne.create_info(["emg1"], 128, ["emg"])
 
+beta = list(range(27, 48))
+alpha = list(range(17, 27))
+delta = list(range(0, 7))
+theta = list(range(7, 17))
+
+ranges = [beta, alpha, delta, theta]
+
 
 def process(X):
     X_eeg1, X_eeg2, X_emg = X
@@ -21,12 +28,15 @@ def process(X):
             raw, fmin=0.5, fmax=24, n_overlap=128
         )
 
+        sums1 = [np.sum(psds[0]) for rng in ranges]
+        sums2 = [np.sum(psds[1]) for rng in ranges]
+
         raw2 = mne.io.RawArray(np.array([X_emg[epoch]]), info2)
         psds2, freqs2 = mne.time_frequency.psd_welch(raw2, fmax=30, picks=["emg1"])
 
         emg_sum = np.sum(psds2[0])
 
-        features.append(np.log([emg_sum] + psds.flatten()))
+        features.append(np.log([emg_sum] + sums1 + sums2))
 
     return features
 
